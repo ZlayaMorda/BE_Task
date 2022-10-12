5693d56d1e50a075a6ed42c2f88ca5f60b13d7a2
ZlayaMorda <tarus01@mail.ru>
2022-10-12 12:35:08 +0300

    page permission

---
+++ apps/page/permissions.py
@@ -0,0 +1,11 @@
+from rest_framework import permissions
+
+
+class IsPageFollower(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user in obj.followers
+
+
+class IsPageNotBlocked(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return not obj.is_blocked
