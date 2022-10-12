0e1e215ac2c202163c9694c7d880509679ca7330
ZlayaMorda <tarus01@mail.ru>
2022-10-12 11:26:01 +0300

    user's permissions

--- apps/user/permissions.py
+++ apps/user/permissions.py
@@ -2,7 +2,6 @@ from rest_framework import permissions
 
 
 class IsAdmin(permissions.BasePermission):
-
     @staticmethod
     def _is_admin(request):
         return request.user.is_staff or request.user.role == request.user.Roles.ADMIN.value
@@ -12,3 +11,28 @@ class IsAdmin(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return self._is_admin(request)
+
+
+class IsModerator(permissions.BasePermission):
+    @staticmethod
+    def _is_moderator(request):
+        return request.user.role == request.user.Roles.MODERATOR.value
+
+    def has_permission(self, request, view):
+        return self._is_moderator(request)
+
+    def has_object_permission(self, request, view, obj):
+        return self._is_moderator(request)
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.owner == request.user
+
+
+class IsNotBlocked(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return not request.user.is_blocked
